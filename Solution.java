7cfbbb9efd9428d51dcb345bc998837fc1c48667
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-19 20:18:12 -0700

    more memory efficient

--- Solution.java
+++ Solution.java
@@ -2,19 +2,18 @@ public class Solution {
     public int[] plusOne(int[] digits) {
         if (digits.length == 0)
             return new int[] { 1 };
-        
-        int carry = 0;
-        for (int index = digits.length - 1; index >= 0; index--) {
-            int sum = carry + digits[index] + 1;
+        int n = digits.length;
+        int carry = 1;
+        for (int index = n - 1; index >= 0; index--) {
+            int sum = carry + digits[index];
             digits[index] = sum % 10;
-            carry = sum > 9 ? 1 : 0;
+            carry = sum / 10;
         }
-        if (carry != 0) {
-            int[] newDigits = new int[digits.length + 1];
+
+        if (carry > 0) {
+            int[] newDigits = new int[n + 1];
             newDigits[0] = carry;
-            for (int index = 0; index < digits.length; index++) {
-                newDigits[index + 1] = digits[index];
-            }
+            System.arraycopy(digits, 0, newDigits, 1, n);
             return newDigits;
         }
         return digits;
