040b2a79292beb2ca405d12fbfe36f8313b9a158
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-19 19:58:12 -0700

    solution found

--- Solution.java
+++ Solution.java
@@ -1,5 +1,22 @@
 public class Solution {
     public int[] plusOne(int[] digits) {
-
-     } 
+        if (digits.length == 0)
+            return new int[] { 1 };
+        
+        int carry = 0;
+        for (int index = digits.length - 1; index >= 0; index--) {
+            int sum = carry + digits[index] + 1;
+            digits[index] = sum % 10;
+            carry = sum > 9 ? 1 : 0;
+        }
+        if (carry != 0) {
+            int[] newDigits = new int[digits.length + 1];
+            newDigits[0] = carry;
+            for (int index = 0; index < digits.length; index++) {
+                newDigits[index + 1] = digits[index];
+            }
+            return newDigits;
+        }
+        return digits;
+    } 
 }
